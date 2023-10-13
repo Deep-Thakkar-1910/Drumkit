60e9609376e38035d0c1a5ca50004f5c6b9dac3b
Deep-Thakkar-1910 <thakkard333@gmail.com>
2023-10-13 22:13:51 +0530

    Version-1

---
+++ index.js
@@ -0,0 +1,73 @@
+// selecting the number of Drum buttons
+var numberOfDrumButtons = document.querySelectorAll('.drum').length;
+
+// looping in order to addEventListener to every drum button.
+for (var i = 0; i < numberOfDrumButtons; i++) {
+   document.querySelectorAll('button')[i].addEventListener("click", playSoundClick);
+}
+
+// playsound through clicks
+function playSoundClick(){
+var buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+}
+
+// Playsound Through keyboard
+
+document.addEventListener("keydown",playSoundKeyboard);
+
+function playSoundKeyboard(){
+
+var key = event.key;
+makeSound(key);
+buttonAnimation(key);
+}
+
+
+
+
+function makeSound(button) {
+   switch (button) {
+      case 'w':
+         var audio = new Audio("sounds/tom-1.mp3");
+         audio.play();
+         break;
+      case 'a':
+         var audio = new Audio("sounds/tom-2.mp3");
+         audio.play();
+         break;
+      case 's':
+         var audio = new Audio("sounds/tom-3.mp3");
+         audio.play();
+         break;
+      case 'd':
+         var audio = new Audio("sounds/tom-4.mp3");
+         audio.play();
+         break;
+      case 'j':
+         var audio = new Audio("sounds/crash.mp3");
+         audio.play();
+         break;
+      case 'k':
+         var audio = new Audio("sounds/snare.mp3");
+         audio.play();
+         break;
+      case 'l':
+         var audio = new Audio("sounds/kick-bass.mp3");
+         audio.play();
+         break;
+
+      default:
+
+   }
+}
+
+function buttonAnimation(cK){
+   var activeButton = document.querySelector("."+cK);
+   activeButton.classList.add("pressed");
+
+   setTimeout(function(){
+      activeButton.classList.remove("pressed");
+   },50);
+}
